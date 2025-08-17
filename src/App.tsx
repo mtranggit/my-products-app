36a338ec523821a1ec8d48261c0936a5fcabfe7e
Michael Trang <michael.trang@hotmail.com>
2025-08-17 06:45:15 +0000

    add basic beer listing with sample data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import {PRODUCTS} from "../data";
+
 function App() {
   return (
     <>
-      <p>Products Listing App!</p>
+      {PRODUCTS.map((product) => (
+        <div key={product.index}>
+          <img alt={product.productName} src={`assets/${product.productImage}`} className="" />
+          <p>Name: {product.productName}</p>
+          <p>Price: {product.price}</p>
+        </div>
+      ))}
     </>
   );
 }
